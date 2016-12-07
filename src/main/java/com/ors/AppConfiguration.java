33a9f311121357c79316bdbb0bd3688690850dc5
Marcin <markowitek@gmail.com>
2016-12-07 18:01:26 +0100

    add message.properties and configuration ResourceBundleMessageSource for messages

---
+++ src/main/java/com/ors/AppConfiguration.java
@@ -0,0 +1,24 @@
+package com.ors;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+
+/**
+ * Created by Marcin on 07.12.2016.
+ */
+@Configuration
+
+public class AppConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("locale/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
