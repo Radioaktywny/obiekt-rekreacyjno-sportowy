992497e70f548ceeea9a8b4a6a71cfb61768c140
Marcin <markowitek@gmail.com>
2016-12-07 22:54:37 +0100

    fix duplicate tables for user in database. create administrator and user

---
+++ src/main/resources/mysql/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO obiekt_rekreacyjno_sportowy.uzytkownik(id, nazwa_uzytkownika, haslo, poziom_dostepu, miasto, ulica, kod_pocztowy, numer_telefonu)
+VALUES('1','administrator','$2a$04$KWeGmrnv.s7rn/zWFN4p/emjZDCdW5pjD6DwsIxaj.GBHnfb6LoF6','ADMINISTRATOR',null,null,null,null);
+
+INSERT INTO obiekt_rekreacyjno_sportowy.uzytkownik(id, nazwa_uzytkownika, haslo, poziom_dostepu, miasto, ulica, kod_pocztowy, numer_telefonu)
+VALUES('2','user123','$2a$04$65my72NWs9afeZ88pN95se8w4CpZUcirwQWd5MG5f.vy8leFGRYWu','USER',null,null,null,null);
+
+# username : administrator, password administrator
+# username : user123, password user123
